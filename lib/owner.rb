6c902ad972002d548284771614502d9a76a3d3cd
Abagail Hudson <abagailanne@gmail.com>
2019-05-28 23:46:50 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,13 @@
 class Owner
   # code goes here
   @@all = []
+  
   attr_reader :species #cant modify directly
   attr_accessor :name, :pets
 
   def initialize(species)
     @species = species
+    @@all << self
   end
 
   def all
