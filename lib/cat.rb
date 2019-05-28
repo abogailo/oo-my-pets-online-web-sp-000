183a7676d4f84928f88b7de031e674e724160957
Abagail Hudson <abagailanne@gmail.com>
2019-05-28 23:40:12 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,7 @@ class Cat
   # code goes here
   attr_reader :name #use a reader so non purposeful name modifications can occur
   attr_accessor :mood
-  
+
   def initialize(name)
     @name = name
     @mood = "nervous"
